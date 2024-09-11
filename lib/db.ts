19d4cabab0483399781f9d5017b9e75364b0445e
Nathan Carter <nathan@and-element.com>
2024-09-11 10:08:47 +0100

    Prisma acc

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-declare global {
-	var prisma: PrismaClient | undefined;
-}
+// Learn more about instantiating PrismaClient in Next.js here: https://www.prisma.io/docs/data-platform/accelerate/getting-started
 
-export const db = globalThis.prisma || new PrismaClient();
+const prismaClientSingleton = () => {
+	return new PrismaClient().$extends(withAccelerate());
+};
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+	prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export const db = prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
