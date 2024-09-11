b966aa91bd83a024ca7356a32cc8be44c75acdcf
Nathan Carter <nathan@and-element.com>
2024-09-11 10:13:54 +0100

    Update import

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 // Learn more about instantiating PrismaClient in Next.js here: https://www.prisma.io/docs/data-platform/accelerate/getting-started
