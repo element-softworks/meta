e42d7c7dc14599049aef8f5f4eae4ec70d9accae
Nathan Carter <nathan@and-element.com>
2024-09-22 20:47:02 +0100

    Docs copy

--- components/docs/documentation-typography.tsx
+++ components/docs/documentation-typography.tsx
@@ -12,12 +12,12 @@ export function DocumentationTypography(props: DocsProps) {
 	}
 
 	const variantClasses = {
-		h1: 'text-3xl md:text-4xl font-semibold mt-12 mb-4',
-		h2: 'text-xl md:text-2xl font-semibold mt-10 mb-2',
-		h3: 'text-lg md:text-xl font-semibold mt-6 mb-2',
-		h4: 'text-base md:text-lg font-semibold mt-4 mb-2',
-		h5: 'text-sm md:text-base font-semibold mt-1 mb-1',
-		h6: 'text-xl md:text-sm font-semibold mt-1 mb-1',
+		h1: 'text-3xl md:text-4xl font-semibold mt-12 mb-4 text-primary',
+		h2: 'text-xl md:text-2xl font-semibold mt-10 mb-2 text-primary',
+		h3: 'text-lg md:text-xl font-semibold mt-6 mb-2 text-primary',
+		h4: 'text-base md:text-lg font-semibold mt-4 mb-2 text-primary',
+		h5: 'text-sm md:text-base font-semibold mt-1 mb-1 text-primary',
+		h6: 'text-xl md:text-sm font-semibold mt-1 mb-1 text-primary',
 		p: 'mt-4 mb-4',
 	};
 
