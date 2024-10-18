b57fe5bc804f5bc721d42250e43d6f5a6e28e78b
Nathan Carter <nathan@and-element.com>
2024-10-18 10:56:50 +0100

    Build err

--- actions/analytics/get-total-sales.ts
+++ actions/analytics/get-total-sales.ts
@@ -55,7 +55,7 @@ export const getTotalSales = async (startDate: string, endDate: string) => {
 	).toFixed(2);
 
 	return {
-		sales: ((invoicesResponse?.total ?? 0) / 100 ?? 0).toFixed(2),
+		sales: ((invoicesResponse?.total ?? 0) / 100).toFixed(2),
 		salesDifference: ((salesDifference ?? 0) / 100 ?? 0).toFixed(2),
 		salesDifferencePercentage:
 			salesDifferencePercentage === 'Infinity' ? 0 : salesDifferencePercentage,
