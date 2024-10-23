aa934e7845bec5fd9e60b0c464b3cb316f95ae4b
Nathan Carter <nathan@and-element.com>
2024-10-23 18:38:13 +0100

    WIP on saasphere

---
+++ app/not-found.tsx
@@ -0,0 +1,44 @@
+import { Navbar } from '@/components/layout/navbar';
+import { Button } from '@/components/ui/button';
+import { SessionProvider } from 'next-auth/react';
+import Link from 'next/link';
+
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+	return {
+		title: `SaaSphere NextJS SaaS Boilerplate`,
+		description:
+			'SaaSphere NextJS SaaS Boilerplate is a modern boilerplate for building SaaS applications quickly.',
+		openGraph: {
+			title: `SaaSphere NextJS SaaS Boilerplate`,
+			description:
+				'SaaSphere NextJS SaaS Boilerplate is a modern boilerplate for building SaaS applications quickly.',
+		},
+		twitter: {
+			title: `SaaSphere NextJS SaaS Boilerplate`,
+			description:
+				'SaaSphere NextJS SaaS Boilerplate is a modern boilerplate for building SaaS applications quickly.',
+		},
+		alternates: {
+			canonical: `${process.env.NEXT_PUBLIC_APP_URL}`,
+		},
+	};
+}
+
+export default function NotFound() {
+	return (
+		<>
+			<SessionProvider>
+				<Navbar />
+				<section className="min-h-[91vh] flex items-center justify-center flex-col gap-4 lg:gap-8 text-center container">
+					<h1 className="font-medium text-3xl sm:text-4xl md:text-6xl lg:text-7xl max-w-[22ch]">
+						Page not found
+					</h1>
+					<p className="text-base lg:text-lg">Could not find requested resource</p>
+					<Link href="/">
+						<Button size="lg">Return home</Button>
+					</Link>
+				</section>
+			</SessionProvider>
+		</>
+	);
+}
