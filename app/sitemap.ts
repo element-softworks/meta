e440b83fbd09f44b013adf451c12f828510ea0cf
Nathan Carter <nathan@and-element.com>
2024-10-25 22:06:08 +0100

    Updates to sitemap priority

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,18 +3,20 @@ import { getAllPostSlugs } from '@/sanity/lib/client';
 export default async function generateSitemaps() {
 	const posts = await getAllPostSlugs();
 
+	console.log(posts, 'all post slugs');
+
 	return [
 		...posts?.map?.((post) => ({
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/blog/${post}`,
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
-			priority: 0.6,
+			priority: 0.8,
 		})),
 		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}`,
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
-			priority: 0.8,
+			priority: 1,
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/docs`,
@@ -32,43 +34,43 @@ export default async function generateSitemaps() {
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard`,
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
-			priority: 0.8,
+			priority: 0.6,
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/login`,
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
-			priority: 0.8,
+			priority: 0.7,
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/register`,
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
-			priority: 0.8,
+			priority: 0.9,
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/error`,
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
-			priority: 0.5,
+			priority: 0.1,
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-password`,
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
-			priority: 0.5,
+			priority: 0.2,
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-verification`,
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
-			priority: 0.5,
+			priority: 0.2,
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/reset`,
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
-			priority: 0.5,
+			priority: 0.2,
 		},
 	];
 }
